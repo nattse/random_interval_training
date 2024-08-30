36921c4b311957d0f927a3e73cca89181662d992
nattse <118491380+nattse@users.noreply.github.com>
2024-08-30 11:48:00 -0400

    Update run_script.py

--- training/box_1-2/run_script.py
+++ training/box_1-2/run_script.py
@@ -117,7 +117,7 @@ def video_setup(log, ardu_thread):
                 ardu_thread.join() # Without this line, we would re-enter the loop and get stuck on video_capture.stderr.readline()
             if ardu_stop.is_set():
                 break
-        print('VS: detected Arduino end signal)
+        print('VS: detected Arduino end signal')
         time.sleep(30)
         print('VS: exited vid recording loop')
         os.kill(vid_id, signal.SIGINT)
